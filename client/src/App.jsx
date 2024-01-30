6fd7712ed1d929a56db3cc4bc5e034bb3abe441c
Shubham <shubhampawar4036@gmail.com>
2024-01-30 16:46:36 +0530

    deployment

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Home } from "./pages/Home";
+import { Home } from "./pages/home";
 import { About } from "./pages/About";
 import { Contact } from "./pages/Contact";
 import { Service } from "./pages/service";
